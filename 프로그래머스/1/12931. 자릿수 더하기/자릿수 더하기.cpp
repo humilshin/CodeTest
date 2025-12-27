44eebed32ba56360152db63151aaa689ce75009d
humilityshin <104838481+humilshin@users.noreply.github.com>
2025-12-27 21:13:24 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+
+    while (1)
+    {
+        if ( n == 0) {
+            return answer;
+        }
+        answer += n % 10;
+        n = n / 10;
+    }
+}
