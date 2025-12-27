20bf7030a78f721546cfc9d394ca68bdf4db75e3
humilityshin <104838481+humilshin@users.noreply.github.com>
2025-12-27 21:02:21 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+
+    if (num % 2 == 0)
+    {
+        return "Even";
+    }
+    else
+    {
+        return "Odd";
+    }
+}
