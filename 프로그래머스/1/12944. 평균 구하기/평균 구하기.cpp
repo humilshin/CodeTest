65891cefbf58cd47ed372caa03d677805f4f7818
humilityshin <104838481+humilshin@users.noreply.github.com>
2025-12-27 21:05:38 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    
+    double sum = 0;
+    for (const auto & entry : arr) {
+        sum += entry;
+    }
+    
+    
+    return sum / arr.size();
+}
