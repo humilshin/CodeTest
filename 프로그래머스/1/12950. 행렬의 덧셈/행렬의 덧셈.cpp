fd8a24fba05a16e3dfabb4e163acea23ead20488
humilityshin <104838481+humilshin@users.noreply.github.com>
2026-01-11 03:21:21 +0900

    [level 1] Title: 행렬의 덧셈, Time: 57.37 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
@@ -8,12 +8,12 @@ using namespace std;
 //size 지정
 // vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
         
-//     int row = arr1[0].size(); // 한 줄의 사이즈
-//     int column = arr1.size(); // 줄들의 개수 
+//     int column = arr1[0].size(); // 한 줄의 사이즈
+//     int row = arr1.size(); // 줄들의 개수 
      
-//     vector<vector<int>> answer(column, vector<int>(row));
-//     for (int i = 0 ; i < column ; ++i ) {
-//         for (int j = 0 ; j < row ; ++j) {
+//     vector<vector<int>> answer(row, vector<int>(column));
+//     for (int i = 0 ; i < row ; ++i ) {
+//         for (int j = 0 ; j < column ; ++j) {
 //             answer[i][j] = arr1[i][j] + arr2[i][j];
 //         }
 //     }
@@ -23,13 +23,13 @@ using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
         
-    int row = arr1[0].size(); // 한 줄의 사이즈
-    int column = arr1.size(); // 줄들의 개수 
+    int column = arr1[0].size(); // 한 줄의 사이즈
+    int row = arr1.size(); // 줄들의 개수 
      
     vector<vector<int>> answer;
-    for (int i = 0 ; i < column ; ++i ) {
+    for (int i = 0 ; i < row ; ++i ) {
         vector<int> temp;
-        for (int j = 0 ; j < row ; ++j) {
+        for (int j = 0 ; j < column ; ++j) {
             temp.push_back(arr1[i][j] + arr2[i][j]);
         }
         answer.push_back(temp);
