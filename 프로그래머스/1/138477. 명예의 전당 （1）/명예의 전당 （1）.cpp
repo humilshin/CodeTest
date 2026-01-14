d8776c950916e6fdaa56579812ee61a659b35bc3
humilityshin <104838481+humilshin@users.noreply.github.com>
2026-01-14 09:39:37 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.09 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -1,26 +1,9 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(int k, vector<int> score) {
     vector<int> answer;
-    vector<int> ranking(k+1);
-    for (int i = 1 ; i <= score.size() ; ++i){
-        int temp = score[i-1];
-        if( i > k) {
-            ranking[k] = temp;
-            sort(ranking.begin(), ranking.end(), greater<int>());
-            answer.push_back(ranking[k-1]);
-        }
-        else {
-            ranking.push_back(temp);
-            sort(ranking.begin(), ranking.end(), greater<int>());
-            answer.push_back(ranking[i-1]);
-        }
-
-
-    }
-    return answer; 
+    return answer;
 }
