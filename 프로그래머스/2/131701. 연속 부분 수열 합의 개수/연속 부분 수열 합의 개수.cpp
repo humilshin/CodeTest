fc243b1392ac852020d5725cd40cb8f4d922824c
humilityshin <104838481+humilshin@users.noreply.github.com>
2026-03-18 20:09:58 +0900

    Update 연속 부분 수열 합의 개수.cpp

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -25,3 +25,33 @@ int solution(vector<int> elements) {
     
     return us.size();
 }
+
+/* two - pointer
+vector<int> solution(vector<int> sequence, int k) {
+    int n = sequence.size();
+    int left = 0, right = 0;
+    long long sum = sequence[0];
+    
+    int ansLeft = 0, ansRight = n - 1; // 최악의 경우 전체 수열
+
+    while (right < n) {
+        if (sum == k) {
+            // 길이가 더 짧으면 갱신
+            if (right - left < ansRight - ansLeft) {
+                ansLeft = left;
+                ansRight = right;
+            }
+            sum -= sequence[left++];
+        }
+        else if (sum < k) {
+            right++;
+            if (right < n) sum += sequence[right];
+        }
+        else { // sum > k
+            sum -= sequence[left++];
+        }
+    }
+
+    return {ansLeft, ansRight};
+}
+*/
